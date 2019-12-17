42fa6369ed200cfbad9c5f64418b927133e4db70
Jeffrey Marshall <m.jeffrey.marshall@gmail.com>
2019-12-17 13:37:43 -0600

    drills

--- src/practice.js
+++ src/practice.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const knex = require('knex');
 
 const knexInstance = knex({
@@ -5,9 +6,121 @@ const knexInstance = knex({
   connection: process.env.DB_URL
 });
 
-knexInstance
+console.log(process.env.DB_URL);
+
+const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
   .from('amazong_products')
-  .select('*')
-  .then(result => {
-    console.log(result);
-  });
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery();
+// .then(result => {
+//   console.log(result)
+// })
+
+function searchByProduceName(searchTerm) {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function paginateProducts(page) {
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' }
+    ])
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function getGroceriesWithName(searchTerm) {
+  knexInstance
+    .select('name')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function paginateGroceries(page) {
+  const productsPerPage = 6;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('id', 'name', 'price', 'category', 'checked')
+    .from('shopping_list')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function groceriesAdded(daysAgo) {
+  knexInstance
+    .select('name')
+    .count('date_added AS added')
+    .where(
+      'date_added',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    )
+    .from('shopping_list')
+    .groupBy('name')
+    .orderBy([{ column: 'name', order: 'ASC' }])
+    .then(result => {
+      console.log(result);
+    });
+}
+
+function costPerCategory() {
+  knexInstance
+    .select('category')
+    .sum('price as total')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log('COST PER CATEGORY');
+      console.log(result);
+    });
+}
+
+costPerCategory();
