39b6b20991d4e140e169fd0796b61de52c4b5936
Jeffrey Marshall <m.jeffrey.marshall@gmail.com>
2019-12-17 23:31:12 -0600

    pus

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+INSERT INTO blogful_articles
+    (title, content, date_published)
+VALUES
+    ('Hello', 'world', '2019-05-05 09:00:00' ),
+    ('welcome', 'home', '2019-12-06 10:00:00' ),
+    ('I', 'am', '2018-05-05 17:00:00' ),
+    ('Jeffrey', 'marshall', '2017-12-05 11:00:00' ),
+    ('Alfred', 'Molina', '2005-11-11 09:09:09' ),
+    ('Spider-Man', 'Tobey Macguire', '2019-05-05 09:00:00' ),
+    ('Wolverine', 'Hugh Jackman', '2019-06-06 19:00:00' ),
+    ('Dark Phoenix', 'terrible', '2019-10-05 09:00:00' ),
+    ('Zoey', 'dog', '2019-04-18 09:00:00' ),
+    ('Dragapult', 'is cool', '2010-12-05 09:59:00' ),
+    ('Zamazenta', 'is beautiful', '2019-11-15 12:00:00' ),
+    ('Pokemon', 'Master', '2019-03-24 03:00:00' ),
+    ('Digimon', 'Didgital World', '2016-06-16 02:00:00' ),
+    ('Goku', 'Saiyan', '2013-03-05 09:58:00' ),
+    ('Vegeta', 'Prince', '2019-12-03 01:00:00' ),
+    ('Broly', 'Monster', '2019-12-04 05:00:00' ),
+    ('kamehame', 'ha', '2019-12-12 12:12:12' ),
+    ('Galick', 'gun', '2019-08-04 04:00:00' ),
+    ('Eraser', 'Cannon', '2012-02-02 02:02:02' ),
+    ('Lapras', 'Gigantamax', '2017-07-07 09:03:02' );
+
+    COMMIT;
+
